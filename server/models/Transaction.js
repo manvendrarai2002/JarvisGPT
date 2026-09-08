9fbf34b72126ee42ef500c86ee3cf93ec09c8efc
Manvendra Rai <manvendrarai002@gmail.com>
2026-09-08 18:44:02 +0530

    security: make payment transactions traceable and idempotent

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-    planId: {type: String, required: true},
-    amount: {type: Number, required: true},
-    credits: {type: Number, required: true},
-    isPaid: {type: Boolean, default: false},
-}, {timestamps: true})
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
+  planId: { type: String, required: true },
+  amount: { type: Number, required: true, min: 0 },
+  credits: { type: Number, required: true, min: 0 },
+  razorpayOrderId: { type: String, required: true, unique: true, index: true },
+  razorpayPaymentId: { type: String, unique: true, sparse: true, index: true },
+  razorpaySignature: { type: String },
+  isPaid: { type: Boolean, default: false, index: true },
+}, { timestamps: true });
 
-const Transaction = mongoose.model('Transaction', transactionSchema);
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
 export default Transaction;
